847ff04f859d4592962baee6ce7b771150d08478
abraham23-978 <cholangau34@gmail.com>
2026-04-16 00:58:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+body {
+  font-family: georgia, serif;
+  background: 	#B22222;
+  color: white;
+  text-align: center;
+  font-style: italic;
+  font-size: 20px;
+}
+
+h1 {
+  font-family: monospace;
+  color: black;
+  font-weight: bold;
+  text-shadow: 4px 4px 2px gray;
+  text-decoration: underline;
+}
+img {
+      border: 1px solid #ddd;
+  border-radius: 8px;
+  width: 300px;
+  border-color: black
+
+}
+h2 {
+  font-family: monospace;
+  color: DodgerBlue ;
+  font-weight: bold;
+  text-shadow: 2px 2px 2px black;
+  font-size: 30px;
+  text-decoration-line: underline;
+}
+h3 {
+   font-family: monospace;
+   color: MediumSeaGreen;
+   font-weight: bold;
+   text-decoration-line: underline;      font-size: 30px;
+   text-shadow: 2px 2px 2px black;
+}
